0ec5774dd75a26bec306c1a664c9be18eb2d5570
Dominik Resl <nikicresl@gmail.com>
2017-02-19 23:43:22 +0100

    styling

--- leonardo_theme_productionready/__init__.py
+++ leonardo_theme_productionready/__init__.py
@@ -4,6 +4,8 @@ LEONARDO_APPS = [
     'leonardo_module_analytics'
 ]
 
-LEONARDO_JS_FILES = []
+LEONARDO_JS_FILES = [
+    'js/productionready.js'
+]
 LEONARDO_SCSS_FILES = []
 LEONARDO_CSS_FILES = []
